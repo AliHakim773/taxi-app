b5d1f022901e7c4dbdb79e244ec7baf34b63fc9d
AliHakim773 <alihakim7732@gmail.com>
2023-12-09 18:35:24 +0200

    added and configered react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
 import "./styles/index.css"
 
+import { Route, Routes } from "react-router-dom"
+
+import SignIn from "./pages/SignIn"
+
 function App() {
-    return <div className='app'></div>
+    return (
+        <div className='app'>
+            <Routes>
+                <Route path='/' element={<SignIn />} />
+            </Routes>
+        </div>
+    )
 }
 
 export default App
---
+++ frontend/src/pages/SignIn/index.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import "./styles.css"
+
+const SignIn = () => {
+    return <div>Sign</div>
+}
+
+export default SignIn
