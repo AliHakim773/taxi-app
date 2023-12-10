6adbba3cdea9cf09bfbf8c25f75717996ad31f83
AliHakim773 <alihakim7732@gmail.com>
2023-12-10 16:56:18 +0200

    finished hero section

--- frontend/src/pages/LandingPage/index.jsx
+++ frontend/src/pages/LandingPage/index.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import "./styles.css"
 import NavBar from "../../components/NavBar"
+import HeroSection from "./HeroSection"
 
 const LandingPage = () => {
     return (
         <div className='page'>
             <NavBar />
+            <HeroSection />
         </div>
     )
 }
