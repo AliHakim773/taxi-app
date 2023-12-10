f6065cfb6a2a4a817401688f460adf87e3c26fe5
AliHakim773 <alihakim7732@gmail.com>
2023-12-10 17:07:26 +0200

    made about us and fixed .page class in utillity.css

--- frontend/src/pages/LandingPage/index.jsx
+++ frontend/src/pages/LandingPage/index.jsx
@@ -2,12 +2,14 @@ import React from "react"
 import "./styles.css"
 import NavBar from "../../components/NavBar"
 import HeroSection from "./HeroSection"
+import AboutUs from "./AboutUs"
 
 const LandingPage = () => {
     return (
         <div className='page'>
             <NavBar />
             <HeroSection />
+            <AboutUs />
         </div>
     )
 }
