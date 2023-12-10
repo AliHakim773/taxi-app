0ee7451b8af3360ad497ad93391051f902d41aee
AliHakim773 <alihakim7732@gmail.com>
2023-12-10 18:20:43 +0200

    added login page basics

---
+++ frontend/src/pages/Login/components/LoginForm/index.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import "./styles.css"
+
+const LoginForm = () => {
+    return <div>hi</div>
+}
+
+export default LoginForm
--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,8 +1,17 @@
 import React from "react"
+import NavBar from "../../components/NavBar"
+import Footer from "../../components/Footer"
 import "./styles.css"
+import LoginForm from "./components/LoginForm"
 
 const Login = () => {
-    return <div>Sign</div>
+    return (
+        <div className='page'>
+            <NavBar />
+            <LoginForm />
+            <Footer />
+        </div>
+    )
 }
 
 export default Login
