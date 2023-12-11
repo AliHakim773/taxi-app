58301abf80318fb2ebd63ae40d1ca2ddc2653165
AliHakim773 <alihakim7732@gmail.com>
2023-12-11 14:36:33 +0200

    made some changes to login

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,14 +1,14 @@
 import React from "react"
 import NavBar from "../../components/common/NavBar"
 import Footer from "../../components/common/Footer"
-import LoginForm from "../../components/LoginPage/LoginForm"
+import LoginForm from "../../components/auth/LoginForm"
 import "./styles.css"
 
 const Login = () => {
     return (
         <div className='page'>
             <NavBar />
-            <main>
+            <main className='login-main'>
                 <LoginForm />
             </main>
             <Footer />
