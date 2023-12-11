67b1e591aafc88b72eb1b434fc902c477d51dddd
NadimRifaii <nadimrifaii3@gmail.com>
2023-12-12 00:41:46 +0200

    implementing the getMessages function in axios folder

--- frontend/src/core/axios/index.js
+++ frontend/src/core/axios/index.js
@@ -1,7 +1,7 @@
 // axiosConfig.js
 import axios from "axios"
 
-export const requestData = async (route, method, data, headers) =>
+export const requestData = async (route, method, data, headers = {}) =>
     await axios
         .request({
             url: `http://127.0.0.1:8000/api/${route}`,
@@ -15,3 +15,14 @@ export const requestData = async (route, method, data, headers) =>
         .then((res) => {
             return res.data
         })
+export const getMessages = async (route, method, data = null, headers = {}) => {
+    await axios
+        .request({
+            url: `http://127.0.0.1:8000/api/${route}`,
+            method,
+            headers: {
+                "Content-Type": "application/json",
+                ...headers
+            }
+        })
+}
