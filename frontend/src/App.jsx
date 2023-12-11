53916e12482e065e1c556deef630ce8114503c33
AliHakim773 <alihakim7732@gmail.com>
2023-12-11 14:36:12 +0200

    added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom"
 
 import Login from "./pages/Login"
 import LandingPage from "./pages/LandingPage"
+import Register from "./pages/Register"
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Routes>
                 <Route path='/' element={<LandingPage />} />
                 <Route path='/login' element={<Login />} />
+                <Route path='/register' element={<Register />} />
             </Routes>
         </div>
     )
