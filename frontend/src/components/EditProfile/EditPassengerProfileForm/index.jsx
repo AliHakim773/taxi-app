efe69634ab0051d1eae36206b8b2237691c4436c
AliHakim773 <alihakim7732@gmail.com>
2023-12-12 09:52:08 +0200

    added import statment

--- frontend/src/components/EditProfile/EditPassengerProfileForm/index.jsx
+++ frontend/src/components/EditProfile/EditPassengerProfileForm/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import defaultPic from "../../../assets/images/Default_pfp.jpg"
 import InputField from "../../common/InputField"
 import Button from "../../common/Button"
