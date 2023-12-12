c011474c2d01568cc3638951f830b37261f4d732
AliHakim773 <alihakim7732@gmail.com>
2023-12-12 08:47:10 +0200

    initialized edit profile form

---
+++ frontend/src/components/EditProfileForm/index.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import "./styles.css"
+
+const EditProfileForm = () => {
+    return <div></div>
+}
+
+export default EditProfileForm
--- frontend/src/pages/EditProfile/index.jsx
+++ frontend/src/pages/EditProfile/index.jsx
@@ -1,13 +1,16 @@
 import React from "react"
 import NavBar from "../../components/common/NavBar"
 import Footer from "../../components/common/Footer"
+import EditProfileForm from "../../components/EditProfileForm"
 import "./styles.css"
 
 const EditProfile = () => {
     return (
         <div className='page'>
             <NavBar />
-            <main className='edit-main'></main>
+            <main className='edit-main'>
+                <EditProfileForm />
+            </main>
             <Footer />
         </div>
     )
