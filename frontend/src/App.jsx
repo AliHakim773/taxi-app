930903e17857f271b14013223ecd1ef8d37730f9
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2023-12-12 17:49:32 +0200

    map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,9 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import LandingPage from "./pages/LandingPage";
 import Register from "./pages/Register";
-
+import ChatRoom from "./pages/ChatRoom";
+import EditProfile from "./pages/EditProfile";
+import CustomerPage from "./pages/CustomerPage";
 
 function App() {
   return (
@@ -14,32 +16,12 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-
+        <Route path="/chatroom" element={<ChatRoom />} />
+        <Route path="/edit-profile" element={<EditProfile />} />
+        <Route path="/customer" element={<CustomerPage />} />
       </Routes>
     </div>
   );
-
-import Login from "./pages/Login"
-import LandingPage from "./pages/LandingPage"
-import Register from "./pages/Register"
-import ChatRoom from "./pages/ChatRoom"
-import EditProfile from "./pages/EditProfile"
-import CustomerPage from "./pages/CustomerPage";
-
-function App() {
-    return (
-        <div className='app'>
-            <Routes>
-                <Route path='/' element={<LandingPage />} />
-                <Route path='/login' element={<Login />} />
-                <Route path='/register' element={<Register />} />
-                <Route path='/chatroom' element={<ChatRoom />} />
-                <Route path='/edit-profile' element={<EditProfile />} />
-                <Route path="/customer" element={<CustomerPage />} />
-            </Routes>
-        </div>
-    )
-
 }
 
 export default App;
