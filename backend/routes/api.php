db42de8e954ae4cc13a75d86043d36578a23c533
Ali Hadi Elchab <alielchab01@gmail.com>
2023-12-13 18:45:51 +0200

    Added admin routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MessageController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AdminAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -34,14 +35,29 @@ Route::controller(MessageController::class)->group(function () {
     Route::get('getMessages', "getAllMessages");
     Route::post('createMessage', 'createMessage');
 });
-Route::controller(MessageController::class)->group(function(){
-    Route::post("getUsersMessages",'getUsersMessages');
-    Route::get('getMessages',"getAllMessages");
-    Route::post('createMessage','createMessage');
+Route::controller(MessageController::class)->group(function () {
+    Route::post("getUsersMessages", 'getUsersMessages');
+    Route::get('getMessages', "getAllMessages");
+    Route::post('createMessage', 'createMessage');
 });
 Route::controller(UserController::class)->group((function () {
     Route::get('get_user', 'get_user');
     Route::post('edit_driver', 'edit_driver');
     Route::post('edit_passenger', 'edit_passenger');
     Route::post('upload_pic', 'upload_pic');
+}));
+
+
+
+Route::prefix('admin/')->group((function () {
+    Route::controller(AdminAuthController::class)->group((function () {
+        Route::post('login', 'login');
+        Route::post('logout', 'logout');
+        Route::post('refresh', 'refresh');
+    }));
+    Route::controller(AdminController::class)->group((function () {
+        Route::get('get_driver_registration_requests', 'getDriverRegisterRequests');
+        Route::get('get_all_drivers', 'getAllDrivers');
+        Route::get('get_all_passengers', 'getAllPassengers');
+    }));
 }));
