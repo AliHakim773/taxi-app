66e6143587c035a328f12fb9ba74fe92c211a7b1
Ali Hadi Elchab <alielchab01@gmail.com>
2023-12-13 18:50:25 +0200

    Linked to backend

--- electron/src/renderer/src/pages/Drivers/index.jsx
+++ electron/src/renderer/src/pages/Drivers/index.jsx
@@ -1,31 +1,55 @@
 import React, { useEffect, useState } from "react";
 import "./style.css";
-import { useLocation } from "react-router-dom";
 import { DriverRequests } from "../../components/DriverRequests";
 import { CurrentUsersTable } from "../../components/CurrentUsersTable";
+import { requestData } from "../../core/axios";
 export const Drivers = () => {
-  const location = useLocation();
   const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    setUsers();
     const token = localStorage.getItem("token");
     const header = {
       Authorization: token,
     };
-
     if (!token) {
       console.error("Token not available");
-      setIsLoggedIn(false);
       return;
     }
-  }, [location.pathname]);
+
+    const getDrivers = async () => {
+      try {
+        const results = await requestData("get_all_drivers", "get", {}, header);
+        setUsers(results.drivers);
+        setIsLoading(false);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getDrivers();
+  }, []);
 
   return (
     <div className="content-container">
       <h1>Drivers</h1>
       <DriverRequests />
-      <CurrentUsersTable />
+      {isLoading ? (
+        <CurrentUsersTable user_role={"driver"} users={loading} />
+      ) : (
+        <CurrentUsersTable user_role="driver" users={users} />
+      )}
+      ;
     </div>
   );
 };
+
+const loading = [
+  {
+    id: "Loading...",
+    name: "Loading...",
+    email: "Loading...",
+    driver: {
+      car: { model: "Loading..." },
+    },
+  },
+];
