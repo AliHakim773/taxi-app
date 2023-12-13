58222263f6098a15382c43dcf31b01d3046d176b
NadimRifaii <nadimrifaii3@gmail.com>
2023-12-13 02:09:36 +0200

    x

--- frontend/src/components/common/NavBar/PfpDropDown/index.jsx
+++ frontend/src/components/common/NavBar/PfpDropDown/index.jsx
@@ -1,36 +1,36 @@
-import React from "react";
-import "./styles.css";
-import { Link, useNavigate } from "react-router-dom";
-import { clearUser } from "../../../../core/redux/user/userSlice";
-import { requestData } from "../../../../core/axios";
-import { useDispatch } from "react-redux";
+import React from "react"
+import "./styles.css"
+import { Link, useNavigate } from "react-router-dom"
+import { clearUser } from "../../../../core/redux/user/userSlice"
+import { requestData } from "../../../../core/axios"
+import { useDispatch } from "react-redux"
 
 const PfpDropDown = ({ isHidden, setIsLoggedIn }) => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
   const handleOnClick = async () => {
     try {
-      const token = localStorage.getItem("token");
+      const token = localStorage.getItem("token")
       const headers = {
         Authorization: token,
-      };
+      }
 
       if (!token) {
-        console.error("Token not available");
-        setIsLoggedIn(false);
-        return;
+        console.error("Token not available")
+        setIsLoggedIn(false)
+        return
       }
-      const res = await requestData("logout", "post", {}, headers);
+      const res = await requestData("logout", "post", {}, headers)
       if (res.status == "success") {
-        localStorage.removeItem("token");
-        dispatch(clearUser(res.user));
-        setIsLoggedIn(false);
-        navigate("/");
+        localStorage.removeItem("token")
+        dispatch(clearUser(res.user))
+        setIsLoggedIn(false)
+        navigate("/")
       }
     } catch (err) {
-      console.log(err);
+      console.log(err)
     }
-  };
+  }
 
   return (
     <div className={isHidden ? "pfp-drop-down" : "pfp-drop-down pfp-show"}>
@@ -44,4 +44,4 @@ const PfpDropDown = ({ isHidden, setIsLoggedIn }) => {
   )
 }
 
-export default PfpDropDown;
+export default PfpDropDown
